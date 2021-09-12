00de1424819f92a2dd7af7a19b9139b912efcdbf
y-n-h-y <upboshi.bigboy@gmail.com>
2021-09-12 03:39:41 +0000

    [add]model

---
+++ db/migrate/20210912033603_create_orders.rb
@@ -0,0 +1,18 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.integer :customer_id
+      t.string :postal_code
+      t.string :address
+      t.string :name
+      t.integer :payment
+      t.integer :price
+      t.integer :postage
+      t.integer :status
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps
+    end
+  end
+end
