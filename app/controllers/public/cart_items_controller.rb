5e3898626eec60a1f6345b547f3621c90d157039
y-n-h-y <upboshi.bigboy@gmail.com>
2021-09-19 13:33:19 +0000

    [Fix]cart_item controller

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -21,6 +21,6 @@ class Public::CartItemsController < ApplicationController
   private
   
   def cart_item_params
-    params.require(:cart_item).permit(:amount, :item_id)
+    params.require(:cart_item).permit(:customer_id, :item_id, :amount)
   end
 end
