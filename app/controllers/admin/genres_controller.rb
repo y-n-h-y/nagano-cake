e5e8b4210a48d0a4f33c3d492f452e28bb37cec3
y-n-h-y <upboshi.bigboy@gmail.com>
2021-10-02 11:47:58 +0000

    [add]validate

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -6,8 +6,12 @@ class Admin::GenresController < ApplicationController
 
   def create
     @genre = Genre.new(genre_params)
-    @genre.save
-    redirect_to admin_genres_path
+    if @genre.save
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.all
+      render :index
+    end
   end
 
   def edit
